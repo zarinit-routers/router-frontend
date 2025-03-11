082736de8ae2f1a8fff76c4f1b6129418855939c
MainMaestro <sargsyan.a917@gmail.com>
2025-03-11 16:04:56 +0300

    header fix

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -1,17 +1,17 @@
 <template>
-    <div class="" id="side_bar">
-        <div class="side_bar_p">
-            <Router-link class="side_bar_settings" to="/home" >Монитор</Router-link>
+    <div class="container mx-auto " id="side_bar">
+        <div class="side_bar_p w-[200px] bg-[#222228] rounded-[10px] p-[20px]">
+            <Router-link class="" to="/home" >Монитор</Router-link>
         </div>
-        <p class="p_setting">Настройки</p>
-        <div class="side_bar_p">
-            <Router-link class="side_bar_settings" to="/system">Система</Router-link>
-            <Router-link class="side_bar_settings" href="{mobiles}">Мобильная связь</Router-link>
-            <Router-link class="side_bar_settings" href="{wifi}">Wi-Fi сеть</Router-link>
-            <Router-link class="side_bar_settings" to="/system">Локальная сеть</Router-link>
-            <Router-link class="side_bar_settings" href="{firewall' %}">Брандмауэр</Router-link>
-            <Router-link class="side_bar_settings" href="{% url 'systemjournal' %}">Системный журнал</Router-link>
-            <Router-link class="side_bar_settings" href="{% url 'map' %}">Карта</Router-link>
+        <p class="w-[200px] p-[20px] ">Настройки</p>
+        <div class="flex flex-col w-[200px] bg-[#222228] rounded-[10px] p-[20px]">
+            <Router-link class="" to="/system">Система</Router-link>
+            <Router-link class="my-[15px]" href="{mobiles}">Мобильная связь</Router-link>
+            <Router-link class="" href="{wifi}">Wi-Fi сеть</Router-link>
+            <Router-link class="my-[15px]" to="/system">Локальная сеть</Router-link>
+            <Router-link class="" href="{firewall' %}">Брандмауэр</Router-link>
+            <Router-link class="my-[15px]" href="{% url 'systemjournal' %}">Системный журнал</Router-link>
+            <Router-link class="" href="{% url 'map' %}">Карта</Router-link>
         </div>
 
     </div>
