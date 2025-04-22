96eb8b25bb8a3007fd3215993085b498e2894218
MainMaestro <sargsyan.a917@gmail.com>
2025-04-22 18:28:13 +0300

    dhcp litle changes

--- src/components/DHCP/DhcpRanges.vue
+++ src/components/DHCP/DhcpRanges.vue
@@ -1,54 +1,68 @@
 <template>
-    <div>
-      <h2>Диапазоны IP-адресов</h2>
-      <div>
-        <p>Текущий диапазон: {{ range.start }} - {{ range.end }}</p>
-        <input v-model="newRangeStart" placeholder="Начальный IP" />
-        <input v-model="newRangeEnd" placeholder="Конечный IP" />
-        <button @click="updateRange">Обновить диапазон</button>
-      </div>
+  <div>
+    <h2>Диапазоны IP-адресов</h2>
+    <div v-if="range.start_ip && range.end_ip">
+      <p>Текущий диапазон: {{ range.start_ip }} - {{ range.end_ip }}</p>
+      <input v-model="newRangeStart" placeholder="Начальный IP" />
+      <input v-model="newRangeEnd" placeholder="Конечный IP" />
+      <button @click="updateRange">Обновить диапазон</button>
     </div>
-  </template>
-  
-  <script>
-  import axios from 'axios';
-  
-  export default {
-    data() {
-      return {
-        range: {},
-        newRangeStart: '',
-        newRangeEnd: ''
+    <div v-else>
+      <p>Диапазон не установлен</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      range: { start_ip: '', end_ip: '' }, // Инициализация с пустыми значениями для start_ip и end_ip
+      newRangeStart: '',
+      newRangeEnd: ''
+    };
+  },
+  methods: {
+    fetchRange() {
+      axios.get('/api/dhcp/ranges')
+        .then(response => {
+          if (response.data.current_range) {
+            this.range = response.data.current_range;
+          } else {
+            this.range = { start_ip: '', end_ip: '' }; // если нет диапазона, инициализируем пустыми значениями
+          }
+        })
+        .catch(error => {
+          console.error(error);
+        });
+    },
+    updateRange() {
+      const data = {
+        start: this.newRangeStart,
+        end: this.newRangeEnd
       };
-    },
-    methods: {
-      fetchRange() {
-        axios.get('/api/dhcp/ranges')
-          .then(response => {
-            this.range = response.data.range;
-          })
-          .catch(error => {
-            console.error(error);
-          });
-      },
-      updateRange() {
-        const data = {
-          start: this.newRangeStart,
-          end: this.newRangeEnd
-        };
-        axios.post('/api/dhcp/ranges', data)
-          .then(() => {
-            alert('Диапазон обновлён');
-            this.fetchRange();
-          })
-          .catch(error => {
-            console.error(error);
-          });
+
+      // Проверка формата IP-адреса
+      const ipPattern = /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/;
+      if (!ipPattern.test(this.newRangeStart) || !ipPattern.test(this.newRangeEnd)) {
+        alert("Некорректный формат IP-адреса");
+        return;
       }
-    },
-    created() {
-      this.fetchRange();
+
+      axios.post('/api/dhcp/ranges', data)
+        .then(() => {
+          alert('Диапазон обновлён');
+          this.fetchRange(); // Обновляем данные после успешного обновления диапазона
+        })
+        .catch(error => {
+          console.error("Ошибка при обновлении диапазона", error);
+        });
     }
-  };
-  </script>
-  
+  },
+  created() {
+    this.fetchRange(); // загружаем диапазон при создании компонента
+  }
+};
+</script>
