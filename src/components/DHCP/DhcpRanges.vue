26303ac8813c939a3acb783abdb12836c6b75e06
MainMaestro <sargsyan.a917@gmail.com>
2025-05-20 15:06:49 +0300

    ranges 3fix

--- src/components/DHCP/DhcpRanges.vue
+++ src/components/DHCP/DhcpRanges.vue
@@ -22,13 +22,29 @@
         placeholder="Конечный IP"
         class="px-3 py-2 rounded bg-gray-700 text-white placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500"
       />
-      <button
-        @click="updateRange"
-        class="bg-blue-600 hover:bg-blue-700 text-white px-4 py-2 rounded transition"
-      >
-        Обновить диапазон
-      </button>
     </div>
+
+    <div class="flex flex-col md:flex-row items-start md:items-center gap-2">
+      <input
+        v-model="optionsRouters"
+        type="text"
+        placeholder="IP шлюза (router)"
+        class="px-3 py-2 rounded bg-gray-700 text-white placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500"
+      />
+      <input
+        v-model="optionsBroadcasts"
+        type="text"
+        placeholder="Broadcast-адрес"
+        class="px-3 py-2 rounded bg-gray-700 text-white placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500"
+      />
+    </div>
+
+    <button
+      @click="updateRange"
+      class="bg-blue-600 hover:bg-blue-700 text-white px-4 py-2 rounded transition"
+    >
+      Обновить диапазон
+    </button>
   </div>
 </template>
 
@@ -46,6 +62,8 @@ const range = ref({
 })
 const newRangeStart = ref('')
 const newRangeEnd = ref('')
+const optionsRouters = ref('')
+const optionsBroadcasts = ref('')
 
 const ipPattern =
   /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/
@@ -56,14 +74,21 @@ const fetchRange = async () => {
     range.value = data.current_range || {}
     newRangeStart.value = range.value.start_ip || ''
     newRangeEnd.value = range.value.end_ip || ''
+    optionsRouters.value = range.value.options_routers || ''
+    optionsBroadcasts.value = range.value.options_broadcasts || ''
   } catch (e) {
     console.error('Ошибка при загрузке диапазона:', e)
   }
 }
 
 const updateRange = async () => {
-  if (!ipPattern.test(newRangeStart.value) || !ipPattern.test(newRangeEnd.value)) {
-    alert('Некорректный формат IP-адреса')
+  if (
+    !ipPattern.test(newRangeStart.value) ||
+    !ipPattern.test(newRangeEnd.value) ||
+    !ipPattern.test(optionsRouters.value) ||
+    !ipPattern.test(optionsBroadcasts.value)
+  ) {
+    alert('Некорректный формат одного из IP-адресов')
     return
   }
 
@@ -78,8 +103,8 @@ const updateRange = async () => {
       netmask: range.value.netmask,
       start_ip: newRangeStart.value,
       end_ip: newRangeEnd.value,
-      options_routers: range.value.options_routers || '',
-      options_broadcasts: range.value.options_broadcasts || ''
+      options_routers: optionsRouters.value,
+      options_broadcasts: optionsBroadcasts.value
     })
     alert('Диапазон обновлён')
     await fetchRange()
