6dcd5173f52c8875a08a2e1eda34d80ba99e6a4a
Katy248 <petrovanton247@gmail.com>
2025-05-23 12:12:36 +0300

    Refactor header

--- src/views/GraphicsView.vue
+++ src/views/GraphicsView.vue
@@ -1,9 +1,9 @@
 <script setup>
-import Header from "../components/TheHeader.vue";
-import DeviceNetworkSpeedDev from "../components/OS/DeviceNetworkSpeed.vue"
+import Header from "../components/layout/TheHeader.vue";
+import DeviceNetworkSpeedDev from "../components/OS/DeviceNetworkSpeed.vue";
 </script>
 
 <template>
   <Header></Header>
-      <DeviceNetworkSpeedDev />
+  <DeviceNetworkSpeedDev />
 </template>
