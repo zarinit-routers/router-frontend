8cfb40c2e550365517a44e179d47d094e91b0a89
Katy248 <petrovanton247@gmail.com>
2025-09-19 12:28:48 +0300

    Fix path

--- src/components/DHCP/DhcpStatusToggle.vue
+++ src/components/DHCP/DhcpStatusToggle.vue
@@ -1,33 +1,35 @@
 <script setup>
-import { onMounted } from 'vue'
-import { Switch } from '@headlessui/vue'
-import { useDhcpStore } from '../../stores/dhcp'
+import { onMounted } from "vue";
+import { Switch } from "@headlessui/vue";
+import { useDhcpStore } from "@/stores/dhcp";
 
-const store = useDhcpStore()
+const store = useDhcpStore();
 
 onMounted(() => {
-  store.fetchStatus()
-})
+  store.fetchStatus();
+});
 </script>
 
 <template>
   <div class="flex items-center space-x-4 p-4 rounded-xl shadow space-y-4">
-
     <p class="w-[170px] mr-2 mb-0">DHCP-сервер:</p>
     <Switch
       :checked="store.enabled"
       @click="store.toggleDHCP"
       :disabled="store.loading"
       class="relative inline-flex h-6 w-11 mb-0 items-center rounded-full transition-colors duration-300 focus:outline-none"
-      :class="store.enabled ? 'bg-green-600' : 'bg-gray-300'">
+      :class="store.enabled ? 'bg-green-600' : 'bg-gray-300'"
+    >
       <span
         class="inline-block h-4 w-4 transform rounded-full bg-white transition-transform duration-300"
-        :class="store.enabled ? 'translate-x-6' : 'translate-x-1'"/>
+        :class="store.enabled ? 'translate-x-6' : 'translate-x-1'"
+      />
     </Switch>
 
-    <span class="text-sm ">
-      DHCP <span :class="store.enabled ? 'text-green-600' : ''">
-        {{ store.enabled ? 'включён' : 'выключен' }}
+    <span class="text-sm">
+      DHCP
+      <span :class="store.enabled ? 'text-green-600' : ''">
+        {{ store.enabled ? "включён" : "выключен" }}
       </span>
     </span>
   </div>
--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,24 @@
 // vite.config.js
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    tailwindcss(),
-  ],
   server: {
     proxy: {
       "/api": { target: "http://localhost:11211", changeOrigin: true },
     },
   },
-})
+  resolve: {
+    alias: {
+      "@": "/src/",
+    },
+  },
+  plugins: [vue(), tailwindcss()],
+  server: {
+    proxy: {
+      "/api": { target: "http://10.81.14.19:11211", changeOrigin: true },
+    },
+  },
+});
