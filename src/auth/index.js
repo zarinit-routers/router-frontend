74f1aec1ad0e92809353f2d8a5c0e6e9b45b9549
Katy248 <petrovanton247@gmail.com>
2025-09-19 18:42:07 +0300

    Added export

--- src/auth/index.js
+++ src/auth/index.js
@@ -4,15 +4,15 @@ import axios from "axios";
 const JWT_TOKEN_NAME = "authToken";
 const DEFAULT_REDIRECT_TO = "/Home";
 const saveToken = (token) => {
-    if (!token) {
-        console.error("Bad token");
-        return;
-    }
-    localStorage.setItem(JWT_TOKEN_NAME, token);
+  if (!token) {
+    console.error("Bad token");
+    return;
+  }
+  localStorage.setItem(JWT_TOKEN_NAME, token);
 };
 
 const getToken = () => {
-    return localStorage.getItem(JWT_TOKEN_NAME);
+  return localStorage.getItem(JWT_TOKEN_NAME);
 };
 
 /**
@@ -22,28 +22,27 @@ const getToken = () => {
  * @param {Boolean|{to: String}} redirect
  */
 const login = (password, redirect, router) => {
-    axios
-      .post("/api/auth/login", { password: password })
-      .then((response) => {
-        saveToken(response.data.token);
-        window.dispatchEvent(new Event("authChanged"));
-        if (redirect && router) {
-          router.push(redirect.to ?? DEFAULT_REDIRECT_TO);
-        }
-      })
-      .catch((err) => {
-        console.error(err);
-      });
-  };
-  
+  axios
+    .post("/api/auth/login", { password: password })
+    .then((response) => {
+      saveToken(response.data.token);
+      window.dispatchEvent(new Event("authChanged"));
+      if (redirect && router) {
+        router.push(redirect.to ?? DEFAULT_REDIRECT_TO);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+};
 
 const isAuthenticated = () => {
-    return !!getToken();
+  return !!getToken();
 };
 
 const logout = () => {
-    localStorage.removeItem(JWT_TOKEN_NAME);
-    window.dispatchEvent(new Event("authChanged")); // Уведомляем о выходе
+  localStorage.removeItem(JWT_TOKEN_NAME);
+  window.dispatchEvent(new Event("authChanged")); // Уведомляем о выходе
 };
 
-export { login, isAuthenticated, logout };
+export { login, isAuthenticated, logout, getToken };
