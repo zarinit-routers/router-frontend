508f4debc5533c6b557d7a3e12d7f1af78cf7990
Katy248 <petrovanton247@gmail.com>
2025-09-22 19:02:31 +0300

    Update API

--- src/api/index.js
+++ src/api/index.js
@@ -1,23 +1,40 @@
 import { getToken } from "../auth";
-import {ref}  from "vue";
+import { ref } from "vue";
 import axios from "axios";
 
+const getCloudStatus = () => {
+  const result = ref({ status: { connected: false } });
 
+  axios
+    .get("/api/cloud/status", {
+      headers: {
+        Authorization: getToken(),
+      },
+    })
+    .then((response) => {
+      result.value = response.data;
+    })
+    .catch((error) => {
+      console.log("Failed get status of cloud connection", error);
+    });
+  return result;
+};
+const getCloudConfig = () => {
+  const result = ref({ passphrase: "", organizationId: "" });
 
+  axios
+    .get("/api/cloud/config", {
+      headers: {
+        Authorization: getToken(),
+      },
+    })
+    .then((response) => {
+      result.value = response.data;
+    })
+    .catch((error) => {
+      console.log("Failed get status of cloud connection", error);
+    });
+  return result;
+};
 
-const getCloudStatus =  () => {
-    const result = ref({status: {connected: false}});
-    
-        axios.get('/api/cloud/status',{
-            headers: {
-                Authorization:  getToken()
-            }
-        }).then(response => {
-            result.value = response.data;
-        });
-        return result;
-    
-}
-
-
-export { getCloudStatus };
+export { getCloudStatus, getCloudConfig };
--- src/components/CloudLogin.vue
+++ src/components/CloudLogin.vue
@@ -1,5 +1,4 @@
 <script setup>
-
 import { ref } from "vue";
 import axios from "axios";
 import { getToken } from "../auth";
@@ -7,33 +6,51 @@ import Input from "./baseComponents/Input.vue";
 import Button from "./baseComponents/Button.vue";
 import { getCloudStatus } from "../api";
 
-
 const password = ref("");
 const organizationId = ref("");
 
-
 const login = async () => {
-    await axios.post("/api/cloud/config",{
-        organizationId: organizationId.value,
-        password: password.value
-
-    }, {headers: {Authorization: getToken()}}  );
-    
+  await axios.post(
+    "/api/cloud/config",
+    {
+      organizationId: organizationId.value,
+      password: password.value,
+    },
+    { headers: { Authorization: getToken() } },
+  );
 };
-
-
+const cloud = getCloudStatus();
 </script>
 
 <template>
-    <div v-if="getCloudStatus().status.connected"> 
-        <h1>Подключение к облаку</h1>
-        <form method="POST" @submit.prevent="login">
-            <p for="id_username">Идентификатор организации</p>
-            <Input v-model="organizationId" type="text" name="username" autofocus="" autocapitalize="none" autocomplete="username" maxlength="150" required="" id="id_username" class="input" />
-            <p for="id_password">Парольная фраза</p>
-            <Input v-model="password" type="password" name="password" autocomplete="current-password" required="" id="id_password" class="input" />
-            <br />
-            <Button type="submit"  class="button">Подключиться к организации</Button>
-        </form>
-    </div>
+  <div v-if="cloud.status.connected">
+    <h1>Подключение к облаку</h1>
+    <form method="POST" @submit.prevent="login">
+      <p for="id_username">Идентификатор организации</p>
+      <Input
+        v-model="organizationId"
+        type="text"
+        name="username"
+        autofocus=""
+        autocapitalize="none"
+        autocomplete="username"
+        maxlength="150"
+        required=""
+        id="id_username"
+        class="input"
+      />
+      <p for="id_password">Парольная фраза</p>
+      <Input
+        v-model="password"
+        type="password"
+        name="password"
+        autocomplete="current-password"
+        required=""
+        id="id_password"
+        class="input"
+      />
+      <br />
+      <Button type="submit" class="button">Подключиться к организации</Button>
+    </form>
+  </div>
 </template>
